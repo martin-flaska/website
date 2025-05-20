fdea7c44d5b85a0f2aba18586d9d8ddda6b37c23
pilot2254 <miso.flaska@gmail.com>
2025-05-20 19:23:11 +0200

    Created a basic Light-Themed website

---
+++ script.js
@@ -0,0 +1,11 @@
+// This file will contain JavaScript functionality
+// For now, it's just a placeholder
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio site loaded")
+
+  // Future functionality will go here
+  // - Dark/light mode toggle
+  // - Animations
+  // - etc.
+})
